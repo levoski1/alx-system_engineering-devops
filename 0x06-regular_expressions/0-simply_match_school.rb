3339c015e92f14114a43f77e7a1af03d9349dc1a
Ugwoke levi <soromtolevi1@gmail.com>
2024-01-31 14:58:30 -0800

    new

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+puts ARGV[0].scan(/[S]chool/).join
+1.
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbttn/).join
+puts ARGV[0].scan(/hbtttn/).join
+puts ARGV[0].scan(/hbttttn/).join
+puts ARGV[0].scan(/hbtttttn/).join
+
