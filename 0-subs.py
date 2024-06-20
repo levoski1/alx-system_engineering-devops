05864fc2b34d7b0180e735d85cdb95d529678ec5
Ugwoke Levi <113690452+levoski1@users.noreply.github.com>
2024-06-20 09:47:50 +0100

    Update and rename refactor{0x16-api_advance}: New changes to 0-subs.py

--- 0-subs.py
+++ 0-subs.py
@@ -20,5 +20,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json().get('data', {})
         return data.get('subscribers', 0)
-    else:
-        return 0
+    return 0
