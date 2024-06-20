776f55f2eb6a40f4f41ec49baba244398fc01dce
Ugwoke levi <113690452+levoski1@users.noreply.github.com>
2024-06-20 09:56:56 +0100

    refactor{0-subs.py and 1-top_ten.py}: New update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API (/rltoken/b-4nD6hwEeNYTwYl5yWNwA) and prints the titles of
-the first 10 hot posts listed for a given subreddit.
-"""
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
-    Fetches the top 10 posts from a given subreddit
-    and prints the titles.
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    Args:
-        subreddit (str): The name of the subreddit.
-    """
-    base_url = ('https://www.reddit.com/r/{}/hot.json'
-                .format(subreddit))
-    params = {'limit': 10}
-    headers = {'User-Agent': 'RedditAPIBot/0.1'}
 
-    response = requests.get(base_url,
-                            headers=headers,
-                            params=params,
-                            allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        for post in data['data']['children']:
-            print(post['data']['title'])
-   
+if __name__ == "__main__":
+    top_ten(argv[1])
